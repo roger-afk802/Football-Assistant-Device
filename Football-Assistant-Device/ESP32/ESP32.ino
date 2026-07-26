d4c9af6477d7ad2e18ad2b5af77babb928701edb
致傑 <lopeter1201@gmail.com>
2026-07-26 15:01:55 +0800

    Update Wi-Fi credentials in ESP32.ino

--- Football-Assistant-Device/ESP32/ESP32.ino
+++ Football-Assistant-Device/ESP32/ESP32.ino
@@ -61,8 +61,8 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 //=====================
 // WiFi
 //=====================
-const char* ssid = "Bee Lo 的 iPhone";
-const char* password = "12345677";
+const char* ssid = "Your Wi-Fi";
+const char* password = "Your Wi-Fi password";
 
 WebServer server(80);
 
